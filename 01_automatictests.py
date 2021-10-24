53751c10e9c650b206409ecfd09ba1b7b10c1525
masa1203 <msys.sawada@gmail.com>
2021-10-24 12:12:58 +0900

    体裁を整えた

--- 01_automatictests.py
+++ 01_automatictests.py
@@ -1,12 +1,31 @@
 import unittest
 
+
 class MyTestCase(unittest.TestCase):
     def test_one(self):
         pass
 
     def notatest(self):
-        # "test"の文字列から始まらないと実行されない
+        """
+        "test"の文字列から始まらないと実行されない
+        """
         pass
 
+
+class MySecondeTestCse(unittest.TestCase):
+    """
+    複数のテストケースを実行できる
+    """
+    def test_two(self):
+        pass
+
+    def test_two_part2(self):
+        """
+        テストケースは細かく分割できる
+        """
+        pass
+
+
 if __name__ == "__main__":
-    unittest.main() # テストランナー関数
+    # テストランナー関数
+    unittest.main()
