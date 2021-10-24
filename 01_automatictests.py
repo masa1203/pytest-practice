6e7322ceaf0aef00671153c195f5844ed414e69d
masa1203 <msys.sawada@gmail.com>
2021-10-24 11:48:13 +0900

    unittestを始めて走らせる

--- 01_automatictests.py
+++ 01_automatictests.py
@@ -3,3 +3,10 @@ import unittest
 class MyTestCase(unittest.TestCase):
     def test_one(self):
         pass
+
+    def notatest(self):
+        # "test"の文字列から始まらないと実行されない
+        pass
+
+if __name__ == "__main__":
+    unittest.main() # テストランナー関数
